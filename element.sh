abf4c9d7d3ae33b69b0013e13bbf707bb4b69173
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2022-12-17 23:54:13 +0000

    fix: remove whitespace from atomic number

--- element.sh
+++ element.sh
@@ -17,7 +17,8 @@ then
     ELEMENT_INFO=$($PSQL "SELECT name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number = properties.atomic_number INNER JOIN types ON properties.type_id = types.type_id WHERE elements.atomic_number = $ATOMIC_NUMBER")
     echo $ELEMENT_INFO | while read NAME LINE SYMBOL LINE TYPE LINE MASS LINE MELTING_POINT LINE BOILING_POINT
     do
-      echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+      ATOMIC_NUMBER_CLEAN=$(echo $ATOMIC_NUMBER | sed 's/ //')
+      echo -e "The element with atomic number $ATOMIC_NUMBER_CLEAN is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     done
   fi
 else
