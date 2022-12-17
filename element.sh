fae4a9008baa7e721a71b3eed4a4acc29cb7ea90
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2022-12-17 23:23:18 +0000

    feat: add reading element info and printing about element functionality

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 if [[ $1 ]]
 then
@@ -10,7 +10,16 @@ then
   else
     ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1' OR name = '$1'")
   fi
-  echo $ATOMIC_NUMBER
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo I could not find that element in the database.
+  else
+    ELEMENT_INFO=$($PSQL "SELECT name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number = properties.atomic_number INNER JOIN types ON properties.type_id = types.type_id WHERE elements.atomic_number = $ATOMIC_NUMBER")
+    echo $ELEMENT_INFO | while read NAME LINE SYMBOL LINE TYPE LINE MASS LINE MELTING_POINT LINE BOILING_POINT
+    do
+      echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done
+  fi
 else
   echo Please provide an element as an argument.
 fi
