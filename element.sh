f03385fc3b34a63d57c1c904734d1e0c1317be22
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2022-12-17 22:44:12 +0000

    feat: added atomic number finder

--- element.sh
+++ element.sh
@@ -4,7 +4,13 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ $1 ]]
 then
-  echo Needs upgrade
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
+  else
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1' OR name = '$1'")
+  fi
+  echo $ATOMIC_NUMBER
 else
   echo Please provide an element as an argument.
 fi
